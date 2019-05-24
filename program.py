e6e015778fc3b5256936f7be30cb17d93e9c76eb
ILYA CHUB <iliachubich@gmail.com>
2019-05-25 01:10:23 +0300

    Добавлена функция округления числа

--- program.py
+++ program.py
@@ -36,50 +36,56 @@ def persent (numb_1):
 
 #возведение в заданную степень
 def vozvedenie_n(numb_1, numb_2):
-result = numb_1 ** numb_2
-return result
+    result = numb_1 ** numb_2
+    return result
 
 #в озведенние в квадрат
 def vozvedenie_2(numb_1):
-result = numb_1 ** 2
-return result
+    result = numb_1 ** 2
+    return result
 
 # возведенние в куб
 def vozvedenie_3(numb_1):
-result = numb_1 ** 3
-return result
+    result = numb_1 ** 3
+    return result
 
 # синус числа
 def sin(numb_1):
-import math
-result = math.sin(numb_1)
-return result
+    import math
+    result = math.sin(numb_1)
+    return result
 
 # косинус числа
 def cos(numb_1):
-import math
-result = math.cos(numb_1)
-return result
+    import math
+    result = math.cos(numb_1)
+    return result
 
 # тангенс числа
 def tang(numb_1):
-import math
-result = math.tan(numb_1)
-return result
+    import math
+    result = math.tan(numb_1)
+    return result
 
 # котангенс числа
 def ctang(numb_1):
-import math
-result = math.cos(numb_1) / math.sin(numb_1)
-return result
+    import math
+    result = math.cos(numb_1) / math.sin(numb_1)
+    return result
 
 # квадратный корень числа
 def sqrt(numb_1):
-import math
-result = math.sqrt(numb_1)
-return result
+    import math
+    result = math.sqrt(numb_1)
+    return result
 
 #n-ый корень числа
 def sqrt_n(numb_1, numb_2):
-result = numb_1 ** (1 / numb_2)
-return result
+    result = numb_1 ** (1 / numb_2)
+    return result
+
+# округление числа
+def rounding (result):
+    if result.is_integer()==Treue:
+        return int(result)
+    return round(result, 10)        
