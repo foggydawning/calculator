aac999314e221355325c4a9cd6f2897f637b9018
ILYA CHUB <iliachubich@gmail.com>
2019-05-25 00:27:21 +0300

    Добавлены функции сложения, вычитания, умножения и деления

---
+++ program.py
@@ -0,0 +1,21 @@
+def addition (numb_1, numb_2):
+    result=numb_1+numb_2
+    import math
+    result=round(result, 10)
+    return result
+
+def subtraction (numb_1, numb_2):
+    result=numb_1-numb_2
+    import math
+    result=round(result, 10)
+    return result
+
+def multiplication (numb_1, numb_2):
+    result=numb_1*numb_2
+    return result
+
+def division (numb_1, numb_2):
+    result=numb_1/numb_2
+    import math
+    result=round(result, 10)
+    return result
